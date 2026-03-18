8f196051584b5fdb4727265a4c407dc9cc1018ab
djnavz1423 <danieljosh_navarro@sjp2cd.edu.ph>
2026-03-18 23:31:58 +0800

    added san container with h2 and p contents for main visualization

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,8 +1,22 @@
 @extends('layouts.admin')
 
 @section('content')
-<div>
-  hello world
-</div>
+<section>
 
+  <div class="container">
+    <h1>Dashboard</h1>
+    <p class="lead">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident, aut.</p>
+  </div>
+
+  <div class="container">
+    <h2>Container2</h2>
+    <p class="lead">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam explicabo placeat dignissimos maxime maiores enim delectus nemo esse eligendi quaerat!</p>
+  </div>
+
+  <div class="container">
+    <h2>Container3</h2>
+    <p class="lead">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam explicabo placeat dignissimos maxime maiores enim delectus nemo esse eligendi quaerat!</p>
+  </div>
+
+</section>
 @endsection
