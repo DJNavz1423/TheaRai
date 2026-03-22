517ddd7316d23bd40c822c7bfd2a7f86d56d3868
djnavz1423 <danieljosh_navarro@sjp2cd.edu.ph>
2026-03-22 22:42:07 +0800

    Added table contents, added @push for inventory css style. Added css styling to inventory heading, add item button, search box, and table

--- resources/views/admin/inventory/inventory.blade.php
+++ resources/views/admin/inventory/inventory.blade.php
@@ -3,41 +3,78 @@
 @section('content')
 <section id="inventory">
  <div class="container">
-    <div class="row mb-5">
+    <div class="row mb-4">
         <h1 class="heading">Item List (1)</h1>
 
-        <button class="btn" type="button">
+        <button id="addButton" class="btn" type="button">
 
         <span class="icon-wrapper">
             <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M440-440H240q-17 0-28.5-11.5T200-480q0-17 11.5-28.5T240-520h200v-200q0-17 11.5-28.5T480-760q17 0 28.5 11.5T520-720v200h200q17 0 28.5 11.5T760-480q0 17-11.5 28.5T720-440H520v200q0 17-11.5 28.5T480-200q-17 0-28.5-11.5T440-240v-200Z"/></svg>
         </span>
-        <span>Add Item</span>
+        <span>Add New Item</span>
         </button>
     </div>
  </div>
 
  <div class="container">
-    <div class="row table-controls">
+    <div class="row table-controls mb-3">
         <div class="searchbox">
             <span class="icon-wrapper">
                 <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M380-320q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l224 224q11 11 11 28t-11 28q-11 11-28 11t-28-11L532-372q-30 24-69 38t-83 14Zm0-80q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z"/></svg>
             </span>
 
-            <input type="text" id="searchBar" placeholder="Search products...">
+            <input type="text" id="searchBar" class="border" placeholder="Search products...">
         </div>
     </div>
 
-    <div class="container table-container">
+    <div class="container table-container border">
         <table>
             <thead>
-                <tr class="border"></tr>
+                <tr>
+                    <th>Item Code</th>
+                    <th>Item Name</th>
+                    <th>Category</th>
+                    <th>Purchase Price</th>
+                    <th>Quantity</th>
+                </tr>
             </thead>
+
+            <tbody>
+                @foreach($ingredients as $item)
+                <tr>
+                    <td>
+                        @empty($item->item_code)
+                        --
+                        @else
+                        <span class="item-data">{{ $item->item_code }}</span>
+                        @endempty
+                    </td>
+                    <td>
+                        <div class="d-flex item-group">
+                            <span class="item-img"></span>
+                            <span class="item-data">
+                                {{ $item->name }}
+                            </span>
+                        </div>
+                    </td>
+                    <td><span class="item-data">{{ $item->category_name }}</span></td>
+                    <td>
+                        <div class="d-flex item-group">
+                            <span class="item-data">&#8369;{{ $item->purchase_price }} / </span>
+                            <span class="unit">{{ $item->primary_unit_abbr }}</span>
+                        </div>
+                    </td>
+                    <td><span class="item-data">{{ $item->stock_quantity }}</span></td>
+                    
+                </tr>
+                @endforeach
+            </tbody>
         </table>
     </div>
  </div>
-</section>
 
 
+<!-- 
 
     <div class="d-flex justify-content-between align-items-center mb-4">
         <h1 class="h2 mb-0">Inventory</h1>
@@ -47,7 +84,7 @@
         </button>
     </div>
 
-    <!-- Table logic remains the same, but add "Edit/Delete" buttons in a new <td> -->
+     Table logic remains the same, but add "Edit/Delete" buttons in a new <td>
     <div class="card shadow-sm border-0">
         <div class="table-responsive">
             <table class="table align-middle">
@@ -95,7 +132,7 @@
     </div>
 </div>
 
-<!-- Add Modal -->
+Add Modal 
 <div class="modal fade" id="addModal" tabindex="-1">
     <div class="modal-dialog">
         <form action="{{ url('/admin/inventory') }}" method="POST" class="modal-content">
@@ -135,4 +172,12 @@
         </form>
     </div>
 </div>
+-->
+</section>
 @endsection
+
+@once
+    @push('styles')
+        <link rel="stylesheet" href="{{ asset('css/dashboard/inventory.css') }}">
+    @endpush
+@endonce
