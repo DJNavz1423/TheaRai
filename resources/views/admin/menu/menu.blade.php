fcecdcf4975e206e819d7f3af4789c536e5ce86f
djnavz1423 <danieljosh_navarro@sjp2cd.edu.ph>
2026-04-01 23:08:19 +0800

    added tabs in menu management modal section

--- resources/views/admin/menu/menu.blade.php
+++ resources/views/admin/menu/menu.blade.php
@@ -114,8 +114,12 @@
                     </div>
                 </div>
                 
-                <div class="container">
-                    <div class="row table-heading"><h3>Recipe & Costing</h3></div>
+                <div class="container tab-container">
+                    <div class="row tab-titles mt-4 mb-1">
+                        <h3>Recipe & Costing</h3>
+                        <h3>Others</h3>
+                    </div>
+                        
                     <div class="container table-container modal-table border">
                         <table>
                             <thead>
@@ -190,29 +194,54 @@
                         </table>
                     </div>
                     
-                    <div class="row">
-                        <div class="input-group">
-                            <span for="q-factor">Q-Factor (10%)</span>
-                            <span id="q-factor-display">&#8369;0.00</span>
+                    <div class="tab-contents active-tab" id="recipe">
+                        <div class="row">
+                            <div class="input-group">
+                                <span for="q-factor">Q-Factor (10%)</span>
+                                <span id="q-factor-display">&#8369;0.00</span>
+                            </div>
+
+                            <div class="input-group">
+                                <label for="margin">Target Margin (%)</label>
+                                <input type="number" name="margin" id="margin" required value="30">
+                            </div>
                         </div>
 
-                        <div class="input-group">
-                            <label for="margin">Target Margin (%)</label>
-                            <input type="number" name="margin" id="margin" required value="30">
+                        <div class="row">
+                            <div class="input-group">
+                                <span>Suggested Price</span>
+                                <span id="suggested-price-display">&#8369;0.00</span>
+                            </div>
+
+                            <div class="input-group">
+                                <label for="final-price">Your Price</label>
+                                <input type="number" name="final_price" id="final-price" required>
+                            </div>
                         </div>
                     </div>
 
-                    <div class="row">
-                        <div class="input-group">
-                            <span>Suggested Price</span>
-                            <span id="suggested-price-display">&#8369;0.00</span>
+                    <div id="others" class="tab-contents">
+                        <div class="row">
+                            <div class="input-group">
+                                <label for="description">Description</label>
+                                <textarea name="description" id="description" placeholder="Enter Description..."></textarea>
+                            </div>
                         </div>
 
-                        <div class="input-group">
-                            <label for="final-price">Your Price</label>
-                            <input type="number" name="final_price" id="final-price" required>
+                        <div class="row">
+                            <div class="input-group">
+                            <label for="image">Add Image</label>
+                                <div class="image-upload">
+                                    <input type="file" accept="image/png,.png,image/jpeg,.jpeg,image/jpg,.jpg" multiple name="img_url" id="image" class="d-none">
+
+                                    <span class="icon-wrapper">
+                                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M440-440ZM120-120q-33 0-56.5-23.5T40-200v-480q0-33 23.5-56.5T120-760h126l50-54q11-12 26.5-19t32.5-7h165q17 0 28.5 11.5T560-800q0 17-11.5 28.5T520-760H355l-73 80H120v480h640v-320q0-17 11.5-28.5T800-560q17 0 28.5 11.5T840-520v320q0 33-23.5 56.5T760-120H120Zm640-640h-40q-17 0-28.5-11.5T680-800q0-17 11.5-28.5T720-840h40v-40q0-17 11.5-28.5T800-920q17 0 28.5 11.5T840-880v40h40q17 0 28.5 11.5T920-800q0 17-11.5 28.5T880-760h-40v40q0 17-11.5 28.5T800-680q-17 0-28.5-11.5T760-720v-40ZM440-260q75 0 127.5-52.5T620-440q0-75-52.5-127.5T440-620q-75 0-127.5 52.5T260-440q0 75 52.5 127.5T440-260Zm0-80q-42 0-71-29t-29-71q0-42 29-71t71-29q42 0 71 29t29 71q0 42-29 71t-71 29Z"/></svg>
+                                    </span>
+                                </div>
+                            </div>
                         </div>
                     </div>
+                    
                 </div>
             </div>
 
