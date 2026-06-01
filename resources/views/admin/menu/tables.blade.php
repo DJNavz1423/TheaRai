02f3248f20c7e84d648a5902da8cde6d62d3ac22
djnavz1423 <danieljosh_navarro@sjp2cd.edu.ph>
2026-06-01 17:38:46 +0800

    action button fixed

--- resources/views/admin/menu/tables.blade.php
+++ resources/views/admin/menu/tables.blade.php
@@ -26,7 +26,7 @@
                     <th>Table Number</th>
                     <th>QR Code URL</th>
                     <th>Print QR</th>
-                    <th>Actions</th>
+                    <th style="text-align: center;">Actions</th>
                 </tr>
             </thead>
             <tbody role="rowgroup">
@@ -46,7 +46,7 @@
                         <form action="{{ route('admin.menu.tables.destroy', $table->id) }}" method="POST" onsubmit="return confirm('Delete this table and deactivate its QR code?');">
                             @csrf
                             @method('DELETE')
-                            <button type="submit" class="btn" style="background: none; border: none; color: var(--danger); padding: 5px;">
+                            <button type="submit" class="btn btn-icon">
                                 <span class="icon-wrapper">
                                     <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M280-120q-33 0-56.5-23.5T200-200v-520q-17 0-28.5-11.5T160-760q0-17 11.5-28.5T200-800h160q0-17 11.5-28.5T400-840h160q17 0 28.5 11.5T600-800h160q17 0 28.5 11.5T800-760q0 17-11.5 28.5T760-720v520q0 33-23.5 56.5T680-120H280Zm200-284 76 76q11 11 28 11t28-11q11-11 11-28t-11-28l-76-76 76-76q11-11 11-28t-11-28q-11-11-28-11t-28 11l-76 76-76-76q-11-11-28-11t-28 11q-11 11-11 28t11 28l76 76-76 76q-11 11-11 28t11 28q11 11 28 11t28-11l76-76Z"/></svg>
                                 </span>
