a4b70579de7b752584b9c3db13ee11fc865ca969
djnavz1423 <danieljosh_navarro@sjp2cd.edu.ph>
2026-09-22 23:46:19 +0800

    transaction view: ADDED currency js fix

--- resources/views/admin/transaction/transaction.blade.php
+++ resources/views/admin/transaction/transaction.blade.php
@@ -169,6 +169,7 @@
   @push('scripts')
   <script type="text/javascript" src="{{ asset('js/tomSelect/tomSelect.js') }}"></script>
   <script type="text/javascript" src="{{ asset('js/tomSelect/tomSelectConfig.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/utils/currency.js') }}" defer></script>
   <script type="text/javascript" src="{{ asset('js/dashboard/toggleDropdown.js') }}" defer></script>
   <script type="text/javascript" src="{{ asset('js/dashboard/filters/tsTransactionFilter.js') }}" defer></script>
   <script type="text/javascript" src="{{ asset('js/dashboard/modal/transactionModal.js') }}" defer></script>
